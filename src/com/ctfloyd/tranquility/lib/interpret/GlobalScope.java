138f48e67b09c2592c857be7b66a8b616b4fef08
ctfloyd <ctfloyd@wisc.edu>
2022-12-13 16:06:09 -0500

    AST: GlobalScope references Console object instead of bypassing it for log
    Now that MemberExpressions support secondary expressions, it's possible to call
    a member of an object that is a function. Update the GlobalScope to more accurately
    reflect this, by requiring callers reference the native 'log' hook by utilizing
    'console.log' instead of 'log' directly.

--- src/com/ctfloyd/tranquility/lib/interpret/GlobalScope.java
+++ src/com/ctfloyd/tranquility/lib/interpret/GlobalScope.java
@@ -3,7 +3,7 @@ package com.ctfloyd.tranquility.lib.interpret;
 public class GlobalScope extends Scope {
 
     public GlobalScope() {
-        put("console", Value.object(new NativeFunction((args) -> new ConsoleObject().log(args))));
+        put("console", Value.object(new ConsoleObject()));
     }
 
 }
