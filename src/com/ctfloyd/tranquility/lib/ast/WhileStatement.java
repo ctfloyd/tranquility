af9dc1338f316ceddb4790c7d0614ec9064bf5a9
ctfloyd <ctfloyd@wisc.edu>
2022-12-14 19:47:47 -0500

    Parser+Interpreter: Support while statements

--- src/com/ctfloyd/tranquility/lib/ast/WhileStatement.java
+++ src/com/ctfloyd/tranquility/lib/ast/WhileStatement.java
@@ -1,2 +1,39 @@
-package com.ctfloyd.tranquility.lib.ast;public class WhileStatement {
+package com.ctfloyd.tranquility.lib.ast;
+
+import com.ctfloyd.tranquility.lib.interpret.AstInterpreter;
+import com.ctfloyd.tranquility.lib.interpret.Value;
+
+import static com.ctfloyd.tranquility.lib.common.Assert.ASSERT;
+
+public class WhileStatement extends AstNode {
+
+    private final AstNode test;
+    private final BlockStatement body;
+
+    public WhileStatement(AstNode test, BlockStatement body) {
+        ASSERT(test != null);
+        ASSERT(body != null);
+        this.test = test;
+        this.body = body;
+    }
+
+    @Override
+    public Value interpret(AstInterpreter interpreter) throws Exception {
+        while(test.interpret(interpreter).asBoolean()) {
+            interpreter.enterScope();
+            body.interpret(interpreter);
+            interpreter.leaveScope();
+        }
+        return Value.undefined();
+    }
+
+    @Override
+    public void dump(int indent) {
+        printIndent(indent);
+        System.out.println("WhileStatement (");
+        test.dump(indent + 1);
+        body.dump(indent + 1);
+        printIndent(indent);
+        System.out.println(")");
+    }
 }
