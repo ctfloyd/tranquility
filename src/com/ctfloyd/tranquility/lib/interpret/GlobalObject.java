bd616f4a32b38e17be2aca148c2a034a2da48800
ctfloyd <ctfloyd@wisc.edu>
2022-12-21 21:58:34 -0600

    GlobalObject: Add undefined to globalThis

--- src/com/ctfloyd/tranquility/lib/interpret/GlobalObject.java
+++ src/com/ctfloyd/tranquility/lib/interpret/GlobalObject.java
@@ -2,8 +2,12 @@ package com.ctfloyd.tranquility.lib.interpret;
 
 public class GlobalObject extends JsObject {
 
+    // 19.1 Value Properties of the Global Object
     public GlobalObject() {
+        // TODO: 19.1.1, 19.1.2, 19.1.3
         put("console", Value.object(new ConsoleObject()));
+        // 19.1.4
+        put("undefined", Value.undefined());
     }
 
 }
