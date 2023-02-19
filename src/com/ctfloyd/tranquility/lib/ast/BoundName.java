44edfe1f6395eb99efd838b22b2a76578cfd5cb9
ctfloyd <ctfloyd@wisc.edu>
2023-02-19 12:22:29 -0600

    Remove unecessary abstract qualifier for BoundName interface

--- src/com/ctfloyd/tranquility/lib/ast/BoundName.java
+++ src/com/ctfloyd/tranquility/lib/ast/BoundName.java
@@ -2,6 +2,6 @@ package com.ctfloyd.tranquility.lib.ast;
 
 import java.util.List;
 
-public abstract interface BoundName {
-    public abstract List<String> getBoundNames();
+public interface BoundName {
+    List<String> getBoundNames();
 }
